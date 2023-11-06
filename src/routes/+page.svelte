8eaacf7bb63709b4a6e2cbad434efe1ceb62bf8c
Sylvan <sylvanfranklin@icloud.com>
2023-11-05 19:03:10 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<main class="w-screen h-screen flex items-center justify-center flex-col">
+  <div class="m-2">
+    <h1 class="w-full text-center">Sylvan Franklin</h1>
+    <p>Weird, beautiful, and interesting web design</p>
+  </div>
+  <div class="w-1/2 aspect-square border-4 border-black rounded-full" />
+</main>
+
